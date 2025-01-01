ee96e245fdd17d2cb4588bd4c265d0b23efe874e
Subham Dubey <111039096+Jenisis-03@users.noreply.github.com>
2025-01-01 23:00:16 +0530

    Completed Responsive Bento Grid

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,8 +6,24 @@ import { MockDiscordUI } from "@/components/mock-discord-ui"
 import { AnimatedList } from "@/components/ui/animated-list"
 import { DiscordMessage } from "@/components/discord-message"
 import Image from "next/image"
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 const Page = () => {
+  const codeSnippet = `await fetch("http://localhost:3000/api/v1/events", {
+    method: "POST",
+    body: JSON.stringify({
+      category: "sale",
+      fields: {
+        plan: "PRO",
+        email: "zoe.martinez2001@email.com",
+        amount: 49.00
+      }
+    }),
+    headers: {
+      Authorization: "Bearer <YOUR_API_KEY>"
+    }
+  })`
+
   return (
     <>
       <section className="relative py-24 sm:py-32 bg-brand-25">
@@ -158,25 +174,78 @@ const Page = () => {
               <div className="absolute inset-px rounded-lg bg-white" />
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
                     Track Any Properties
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Add any Custom data you like to an event , Such as a user email , a purchase amount or an exceeded quota.
+                    Add any Custom data you like to an event , Such as a user
+                    email , a purchase amount or an exceeded quota.
                   </p>
                 </div>
                 <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
                   <Image
                     className="w-full max-lg:max-w-xs"
                     src="/bento-custom-data.png"
-                    alt="Bento Box Illustrating Custom Data tracking" 
+                    alt="Bento Box Illustrating Custom Data tracking"
                     width={500}
                     height={300}
                   />
                 </div>
-
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"/>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+            </div>
+            {/* FOURTH BENTO GRID */}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
+              <div
+                className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]
+ max-lg:rounded-b-[calc(2rem+1px)] ;g:rounded-r-[calc(2rem+1px)]"
+              >
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Easy to Integrate
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Connect SaaSCord with your Saas in minutes. No coding is
+                    required for this functionality to work properly and works
+                    as expected with the help of the API.
+                  </p>
+                </div>
+                <div className="relative min-h-[30rem] w-full grow">
+                  <div className="absolute bottom-0 left-10 right-0 top-10 overflown-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                      <div className="-mb-px flex text-sm/6 font medium text-gray-400">
+                        <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                          SaaScord.js
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="overflow-hidden">
+                      <div className="max-h-[30rem]">
+                        <SyntaxHighlighter
+                          language="typescript"
+                          style={{
+                            ...oneDark,
+                            'pre[class*="language-"]': {
+                              ...oneDark['pre[class*="language-"]'],
+                              background: "transperent",
+                              overflow: "hidden",
+                            },
+                            'code[class*="language-"]': {
+                              ...oneDark['code[class*="language-"]'],
+                              background: "transperent",
+                            },
+                          }}
+                        >
+                          {codeSnippet}
+                        </SyntaxHighlighter>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
             </div>
           </div>
         </MaxWidthWrapper>
@@ -184,7 +253,7 @@ const Page = () => {
       </section>
 
       <section></section>
-      <section></section>
+      
     </>
   )
 }
